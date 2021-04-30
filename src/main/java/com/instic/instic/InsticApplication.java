cc3c9b6aea26a0f15c493bb85ca2035ed766334b
Maël Martin <mmartin@atelier-chevara.com>
2021-04-30 11:38:37 +0200

    Initial commit

---
+++ src/main/java/com/instic/instic/InsticApplication.java
@@ -0,0 +1,13 @@
+package com.instic.instic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instic/instic/InsticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instic.instic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
