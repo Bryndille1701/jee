d3f6072ad9a2ebec7eb66f800a134fbd51158427
Maël Martin <mmartin@atelier-chevara.com>
2021-04-30 09:37:12 +0200

    Initial commit

---
+++ src/test/java/com/instic/jeeproject/JeeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instic.jeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
